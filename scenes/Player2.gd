5ea5252bb2bcc75d8ade91407814e009faf25cc4
Extremos <96099928+ProjectEXT@users.noreply.github.com>
2022-11-08 13:11:31 -0500

    Added Player2 movement

--- scenes/Player2.gd
+++ scenes/Player2.gd
@@ -4,7 +4,7 @@ var speed = 400
 
 func _physics_process(delta):
 	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("player1_up"):
+	if Input.is_action_pressed("player2_up"):
 		velocity.y -= 1
 	if Input.is_action_pressed("player2_down"):
 		velocity.y += 1
