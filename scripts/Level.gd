6235a8b9631db4e4ed31ef734e1251abf5addecd
Extremos <96099928+ProjectEXT@users.noreply.github.com>
2022-11-08 14:10:21 -0500

    Added Scoring System

--- scripts/Level.gd
+++ scripts/Level.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
-
+var PlayerScore = 0
+var ComputerScore = 0
 
 func _on_LeftGoal_body_entered(body):
 	$Ball.position = Vector2(640, 360)
-
+	ComputerScore +=1
 
 func _on_RightGoal_body_entered(body):
 	$Ball.position = Vector2(640, 360)
+	PlayerScore +=1
+	
+func _process(delta):
+	$Scoreboard/PlayerScore.text = "Player 1 :   " + str(PlayerScore)
+	$Scoreboard/ComputerScore.text = str(ComputerScore) + "   : Computer"
