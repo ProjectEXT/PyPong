11f5236e5faae542aef38e122b3f3cba7d7c1a08
Extremos <96099928+ProjectEXT@users.noreply.github.com>
2022-11-08 14:32:27 -0500

    Added Countdown
    BUG: The ball... has a mind of its own? The ball will react * bounce off of nothing randomly, changing its direction. I kind of like this bug if I'm being honest, It might be able to be turned into its own game spinoff.

--- scripts/AIlevel.gd
+++ scripts/AIlevel.gd
@@ -3,16 +3,24 @@ extends Node2D
 var PlayerScore = 0
 var ComputerScore = 0
 
-
 func _on_LeftGoal_body_entered(body):
-	$Ball.position = Vector2(640, 360)
+	scored_achieved()
 	ComputerScore +=1
-
-
+	
 func _on_RightGoal_body_entered(body):
-	$Ball.position = Vector2(640, 360)
+	scored_achieved()
 	PlayerScore += 1
 	
 func _process(delta):
 	$Scoreboard/PlayerScore.text = "Player 1 :   " + str(PlayerScore)
 	$Scoreboard/ComputerScore.text = str(ComputerScore) + "   : Computer"
+
+func _on_Timer_timeout():
+	$Ball.start_ball()
+	
+func scored_achieved():
+	$Ball.position = Vector2(640, 360)
+	$Ball.stop_ball()
+	$Timer.start()
+	$Player.position = Vector2(15, 360)
+	$Computer.position = Vector2(1265, 360)
--- scripts/Level.gd
+++ scripts/Level.gd
@@ -1,16 +1,26 @@
 extends Node2D
 
 var PlayerScore = 0
-var ComputerScore = 0
+var Player2Score = 0
 
 func _on_LeftGoal_body_entered(body):
-	$Ball.position = Vector2(640, 360)
-	ComputerScore +=1
-
+	scored_achieved()
+	Player2Score +=1
+	
 func _on_RightGoal_body_entered(body):
-	$Ball.position = Vector2(640, 360)
-	PlayerScore +=1
+	scored_achieved()
+	PlayerScore += 1
 	
 func _process(delta):
 	$Scoreboard/PlayerScore.text = "Player 1 :   " + str(PlayerScore)
-	$Scoreboard/ComputerScore.text = str(ComputerScore) + "   : Computer"
+	$Scoreboard/Player2Score.text = str(Player2Score) + "   : Player 2"
+
+func _on_Timer_timeout():
+	$Ball.start_ball()
+	
+func scored_achieved():
+	$Ball.position = Vector2(640, 360)
+	$Ball.stop_ball()
+	$Timer.start()
+	$Player.position = Vector2(15, 360)
+	$Player2.position = Vector2(1265, 360)
