df06b8857ce3322526f2b53fbdedef386228b6a8
Extremos <96099928+ProjectEXT@users.noreply.github.com>
2022-11-08 09:24:12 -0500

    Top & Bottom Borders
    CollisionBody's added to prevent the player from moving off of the screen

---
+++ scripts/Player.gd
@@ -0,0 +1,11 @@
+extends KinematicBody2D
+
+var speed = 400
+
+func _physics_process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	move_and_slide(velocity)
